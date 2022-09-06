40d6818d56da8b35615aa00a0688c7001050b50b
Bagus Facsi Aginsa <910155@telkom.co.id>
2022-09-06 09:45:16 +0700

    fix bug cannot read env

--- app1/src/app.js
+++ app1/src/app.js
@@ -3,9 +3,13 @@ const cron = require('node-cron');
 const { PORT, IPBIND } = require("./configs");
 const redisMsgQueue = require("./models/redisMsgQueue");
 
+let counter = 1;
+
 cron.schedule("* * * * * *", async () => {
-    const message = "I want a pizza"
-    await redisMsgQueue(message)
+    const message = "I want a pizza "
+
+    await redisMsgQueue(message + counter)
+    counter++
 })
 
 app.post("/notification", async (req,res) => {
--- app1/src/configs/index.js
+++ app1/src/configs/index.js
@@ -1,7 +1,9 @@
+require("dotenv").config()
+
 module.exports = {
     PORT: parseInt(process.env.PORT) || 3000,
     IPBIND: process.env.IPBIND || "0.0.0.0",
-    REDIS_HOST: process.env.APP2_URL || "127.0.0.1",
+    REDIS_HOST: process.env.REDIS_HOST || "127.0.0.1",
     REDIS_PORT: parseInt(process.env.REDIS_PORT) || 6379,
     REDIS_PASS: process.env.REDIS_PASS
 }
